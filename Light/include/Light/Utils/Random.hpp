2b213017c9c34646c0f696f28842a9e48c1c853b
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2023-06-01 21:39:01 +0200

    Replaced rand with c++ random. Fixed debug/release differences.

--- Light/include/Light/Utils/Random.hpp
+++ Light/include/Light/Utils/Random.hpp
@@ -1,12 +1,36 @@
 #pragma once
 
-#include <cstdlib>
+#include <random>
+
+#include "glm/glm.hpp"
+#include "glm/gtx/norm.hpp"
 
 namespace Light::Utils{
 
-template<typename T>
-T random(){
-    return T(rand()) / T(RAND_MAX);
+namespace Detail{
+    thread_local std::uniform_real_distribution<double> uniformDistribution01(0.0, 1.0);
+    thread_local std::mt19937 rngGenerator;
+}
+
+// returns a random value in [0, 1]
+inline float random(){
+    return Detail::uniformDistribution01(Detail::rngGenerator);
+}
+
+glm::vec3 randomInUnitSphere(){
+    glm::vec3 v;
+    do {
+        v = {
+            random()*2.0-1.0,
+            random()*2.0-1.0,
+            random()*2.0-1.0,
+        };
+    } while(glm::length2(v) > 1);
+    return v;
+}
+
+glm::vec3 randomOnUnitSphere(){
+    return glm::normalize(randomInUnitSphere());
 }
 
 }
