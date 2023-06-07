b1ff4727795c7211dd5109e6e12ce57e4c44d208
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2023-06-07 23:04:46 +0200

    Added antialiasing.

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ use ultraviolet::{Vec3, Vec4};
 
 use crate::sphere::Sphere;
 
+use rand::Rng;
+
 fn lerp<T>(t: f32, x0: T, x1: T) -> T
     where T: Add<T, Output = T> + Mul<f32, Output = T>{
         x0*(1.0-t) + x1*t
@@ -43,7 +45,7 @@ fn trace_ray(ray: Ray, scene: &impl Hittable) -> Vec3{
 
 fn main() {
     let mut image: Image = Image::new(400, 225);
-
+    let samples_per_pixel: usize = 100;
 
     let scene: Vec<Box<dyn Hittable>> = vec![
         Box::new(Sphere{
@@ -58,8 +60,9 @@ fn main() {
         })
     ];
 
+    let mut rng = rand::thread_rng();
 
-    println!("Rendering {}x{} image...", image.width(), image.height());
+    println!("Rendering {}x{} image @ {} spp...", image.width(), image.height(), samples_per_pixel);
     let rendering_start = Instant::now();
 
     let aspect_ratio = image.width() as f32 / image.height() as f32;
@@ -67,25 +70,30 @@ fn main() {
     let inverse_camera_matrix = camera_matrix.inversed();
     for x in 0..image.width(){
         for y in 0..image.height(){
+            for _sample in 0..samples_per_pixel{
+                let x_offset: f32 = rng.gen(); 
+                let y_offset: f32 = rng.gen(); 
 
-            let ray: Ray = Ray{
-                direction: (inverse_camera_matrix * Vec4::new(
-                    (x as f32 / image.width() as f32) * 2.0 - 1.0,
-                    (y as f32 / image.height() as f32) * 2.0 - 1.0,
-                    1.0,
-                    1.0
-                )).xyz(), 
+                let ray: Ray = Ray{
+                    direction: (inverse_camera_matrix * Vec4::new(
+                        ((x as f32 + x_offset) / image.width() as f32) * 2.0 - 1.0,
+                        ((y as f32 + y_offset) / image.height() as f32) * 2.0 - 1.0,
+                        1.0,
+                        1.0
+                    )).xyz(), 
 
-                origin: Vec3::new(0.0,0.0,0.0)
-            };
+                    origin: Vec3::new(0.0,0.0,0.0)
+                };
 
-            image[(x, y)] = trace_ray(ray, &scene);
+                image[(x, y)] += trace_ray(ray, &scene);
+            }
+            image[(x, y)] /= samples_per_pixel as f32;
         }
         print!("[{:<50}]\r", "#".repeat((x*50 / (image.width()-1)) as usize));
         stdout().flush().unwrap();
     }
     println!();
     println!("Rendering took {:.2?}", rendering_start.elapsed());
-    
+
     image.save_to_file("/tmp/test.ppm").unwrap();
 }
