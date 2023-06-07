d40509df0fb496d09e7dd248a0afa60a4c370a83
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2023-06-07 23:57:42 +0200

    Started multithreading.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{time::Instant, io::{stdout, Write}, ops::{Mul, Add}};
+use std::{time::Instant, io::{stdout, Write}, ops::{Mul, Add, AddAssign}, sync::Mutex, thread::{Thread, self}};
 
 use hit_result::HitResult;
 use hittable::Hittable;
@@ -17,6 +17,8 @@ use ultraviolet::{Vec3, Vec4};
 use crate::sphere::Sphere;
 
 use rand::Rng;
+
+use rayon::prelude::*;
 
 fn lerp<T>(t: f32, x0: T, x1: T) -> T
     where T: Add<T, Output = T> + Mul<f32, Output = T>{
@@ -44,10 +46,12 @@ fn trace_ray(ray: Ray, scene: &impl Hittable) -> Vec3{
 }
 
 fn main() {
-    let mut image: Image = Image::new(400, 225);
+    let image_width: i32 = 400;
+    let image_height: i32 = 225;
+    let protected_image: Mutex<Image> = Mutex::new(Image::new(image_width, image_height));
     let samples_per_pixel: usize = 100;
 
-    let scene: Vec<Box<dyn Hittable>> = vec![
+    let scene: Vec<Box<dyn Hittable + Sync>> = vec![
         Box::new(Sphere{
             center: Vec3::new(0.0,0.0,-1.0),
             radius: 0.5,
@@ -60,24 +64,35 @@ fn main() {
         })
     ];
 
-    let mut rng = rand::thread_rng();
+    // setup rendering data
 
-    println!("Rendering {}x{} image @ {} spp...", image.width(), image.height(), samples_per_pixel);
-    let rendering_start = Instant::now();
-
-    let aspect_ratio = image.width() as f32 / image.height() as f32;
+    let aspect_ratio = image_width as f32 / image_height as f32;
     let camera_matrix = ultraviolet::projection::perspective_gl((90.0 as f32).to_radians(), aspect_ratio, 0.0001, 10000.0);
     let inverse_camera_matrix = camera_matrix.inversed();
-    for x in 0..image.width(){
-        for y in 0..image.height(){
+    let scanlines = (0..image_height).collect::<Vec<i32>>();
+    let progress: Mutex<usize> = Mutex::new(0);
+
+    println!("Rendering {}x{} image @ {} spp...", image_width, image_height, samples_per_pixel);
+    
+    let progress_bar_thread = thread::spawn(|| {
+        while progress.lock().unwrap() < image_height{
+        }
+    });
+    progress_bar_thread.
+
+    let rendering_start = Instant::now();
+    scanlines.par_iter().for_each(|y: &i32|{ 
+        let mut rng = rand::thread_rng();
+        for x in 0..image_width{
+            let mut local_pixel: Vec3 = Vec3::new(0.0, 0.0, 0.0);
             for _sample in 0..samples_per_pixel{
                 let x_offset: f32 = rng.gen(); 
                 let y_offset: f32 = rng.gen(); 
 
                 let ray: Ray = Ray{
                     direction: (inverse_camera_matrix * Vec4::new(
-                        ((x as f32 + x_offset) / image.width() as f32) * 2.0 - 1.0,
-                        ((y as f32 + y_offset) / image.height() as f32) * 2.0 - 1.0,
+                        ((x as f32 + x_offset) / image_width as f32) * 2.0 - 1.0,
+                        ((*y as f32 + y_offset) / image_height as f32) * 2.0 - 1.0,
                         1.0,
                         1.0
                     )).xyz(), 
@@ -85,15 +100,14 @@ fn main() {
                     origin: Vec3::new(0.0,0.0,0.0)
                 };
 
-                image[(x, y)] += trace_ray(ray, &scene);
+                local_pixel += trace_ray(ray, &scene);
             }
-            image[(x, y)] /= samples_per_pixel as f32;
+            protected_image.lock().unwrap()[(x, *y)] = local_pixel / samples_per_pixel as f32;
         }
-        print!("[{:<50}]\r", "#".repeat((x*50 / (image.width()-1)) as usize));
-        stdout().flush().unwrap();
-    }
+        progress.lock().unwrap().add_assign(1);
+    });
     println!();
     println!("Rendering took {:.2?}", rendering_start.elapsed());
 
-    image.save_to_file("/tmp/test.ppm").unwrap();
+    protected_image.lock().unwrap().save_to_file("/tmp/test.ppm").unwrap();
 }
