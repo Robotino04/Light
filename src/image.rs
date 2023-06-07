fa42a645aa46e734366cd92fd8bca531e706fa62
Robotino04 <roman+github@moonsmile.ch>
2023-06-07 10:38:57 +0200

    Added background gradient.

--- src/image.rs
+++ src/image.rs
@@ -1,15 +1,10 @@
 use std::{ops::{Index, IndexMut}, path::Path, fs::File, io::Write, slice};
 
-#[derive(Debug, Clone, Copy)]
-pub struct Pixel{
-    pub r: f32,
-    pub g: f32,
-    pub b: f32,
-}
+use ultraviolet::Vec3;
 
 #[derive(Debug, Clone)]
 pub struct Image{
-    pub pixels: Vec<Pixel>,
+    pixels: Vec<Vec3>,
     width: i32,
     height: i32,
 }
@@ -17,7 +12,7 @@ pub struct Image{
 impl Image{
     pub fn new(width: i32, height:i32) -> Image{
         return Image{
-            pixels: vec![Pixel{r: 0.0, g: 0.0, b: 0.0}; (width*height) as usize],
+            pixels: vec![Vec3{x: 0.0, y: 0.0, z: 0.0}; (width*height) as usize],
             width: width,
             height: height,
         };
@@ -28,10 +23,13 @@ impl Image{
         let mut file = File::create(& path)?;
         write!(file, "P6\n{} {} 255\n", self.width, self.height)?;
 
-        for pixel in self.pixels.iter(){
-            file.write(slice::from_ref(&((pixel.r * 255.0).clamp(0.0, 255.0) as u8)))?;
-            file.write(slice::from_ref(&((pixel.g * 255.0).clamp(0.0, 255.0) as u8)))?;
-            file.write(slice::from_ref(&((pixel.b * 255.0).clamp(0.0, 255.0) as u8)))?;
+        for pixel in self.pixels.iter().rev(){
+            let mut pixel_mut: Vec3 = *pixel;
+            pixel_mut.clamp(Vec3::new(0.0, 0.0, 0.0), Vec3 { x: 1.0, y: 1.0, z: 1.0});
+            pixel_mut *= 255.0;
+            file.write(slice::from_ref(&(pixel_mut.x as u8)))?;
+            file.write(slice::from_ref(&(pixel_mut.y as u8)))?;
+            file.write(slice::from_ref(&(pixel_mut.z as u8)))?;
         }
 
         return Ok(());
@@ -42,14 +40,14 @@ impl Image{
 }
 
 impl Index<(i32, i32)> for Image{
-    type Output = Pixel;
-    fn index(&self, idx: (i32, i32)) -> &Pixel{
+    type Output = Vec3;
+    fn index(&self, idx: (i32, i32)) -> &Vec3{
         return self.pixels.index((idx.0 + idx.1 * self.width) as usize);
     }
 }
 
 impl IndexMut<(i32, i32)> for Image{
-    fn index_mut(&mut self, idx: (i32, i32)) -> &mut Pixel{
+    fn index_mut(&mut self, idx: (i32, i32)) -> &mut Vec3{
         return self.pixels.index_mut((idx.0 + idx.1 * self.width) as usize);
     }
 }
