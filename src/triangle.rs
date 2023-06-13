ebb239d5bb9c2c92028556c5963da42a0602c6b0
Robotino04 <roman+github@moonsmile.ch>
2023-06-13 18:00:20 +0200

    Added normal smoothing.

--- src/triangle.rs
+++ src/triangle.rs
@@ -2,7 +2,7 @@ use ultraviolet::{Vec3, Vec2};
 
 use crate::{hittable::Hittable, ray::Ray, hit_result::HitResult};
 
-#[derive(Default)]
+#[derive(Default, Clone, Copy)]
 pub struct Triangle{
     pub vertices: [Vec3; 3], 
     pub normals: [Vec3; 3],
@@ -12,9 +12,9 @@ pub struct Triangle{
 impl Hittable for Triangle{
     // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
     fn hit(&self, ray: Ray, hit: &mut HitResult, min_distance: f32) -> bool{
-        const EPSILON: f32 = 1e-5;
-        let edge1 = self.vertices[1] - self.vertices[0];
-        let edge2 = self.vertices[2] - self.vertices[0];
+        const EPSILON: f32 = 1e-8;
+        let edge1 = self.vertices[1] - self.vertices[2];
+        let edge2 = self.vertices[0] - self.vertices[2];
         let h = ray.direction.cross(edge2);
         let a = edge1.dot(h);
 
@@ -23,7 +23,7 @@ impl Hittable for Triangle{
         }
 
         let f = 1.0 / a;
-        let s = ray.origin - self.vertices[0];
+        let s = ray.origin - self.vertices[2];
         let u = f * s.dot(h);
 
         if u < 0.0 || u > 1.0{
@@ -42,7 +42,20 @@ impl Hittable for Triangle{
 
         if t > min_distance && t < hit.t { // ray intersection
             hit.t = t;
-            hit.normal = self.normals[0];
+            
+            let hit_point = ray.at(t);
+            let face_normal = edge2.cross(edge1);
+
+            let mut interpolation_vec = Vec3{
+            x: face_normal.dot(edge1.cross(self.vertices[1] - hit_point)),
+            y: face_normal.dot(edge2.cross(hit_point - self.vertices[2])),
+            z: 0.0,
+            };
+            interpolation_vec /= face_normal.mag_sq();
+            interpolation_vec.z = 1.0 - interpolation_vec.x - interpolation_vec.y;
+
+            hit.normal = (self.normals[0] * interpolation_vec.x + self.normals[1] * interpolation_vec.y + self.normals[2] * interpolation_vec.z).normalized();
+  
             return true;
         }
         else{ // This means that there is a line intersection but not a ray intersection.
