5c6eee4e029dcf7bed607af47e6a3a14dd7707ae
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2023-06-21 11:06:58 +0200

    Made cli load blender exports.

--- light-cli/src/main.rs
+++ light-cli/src/main.rs
@@ -1,84 +1,38 @@
-use std::{time::Instant, io::{stdout, Write}, sync::{Mutex, Arc}, thread};
-use light::{mesh::Mesh, image::Image, hittable::Hittable, sphere::Sphere, material::Material, camera::Camera, trace_ray};
+use std::{time::Instant, io::{stdout, Write}, sync::{Mutex, Arc, atomic::AtomicUsize}, thread};
+use light::{image::Image, trace_ray, importing::load_from_blender, image_filters::{average_samples, gamma_correct}};
 use rayon::prelude::*;
-use ultraviolet::{self, Mat4, Vec3};
+use ultraviolet::Vec3;
 
 fn main() {
-    let mut cube = Mesh::from_obj("meshes/default_cube.obj").unwrap();
-    cube.apply_matrix(Mat4::from_translation(Vec3::new(1.0, 0.0, -1.0)) * Mat4::from_scale(0.9));
-
-    let protected_image: Arc<Mutex<Image>> = Arc::new(Mutex::new(Image::new(1920, 1080)));
-    let image_width: i32 = protected_image.lock().unwrap().width();
-    let image_height: i32 = protected_image.lock().unwrap().height();
+    let scene = load_from_blender("/tmp/blender_export.toml").unwrap(); 
+   
+    let protected_image: Arc<Mutex<Image>> = Arc::new(Mutex::new(Image::new(scene.width, scene.height)));
     let samples_per_pixel: usize = 1000;
     let max_depth: i32 = 50;
 
-    let scene: Vec<Box<dyn Hittable + Sync + Send>> = vec![
-        Box::new(Sphere{
-            center: Vec3::new(-1.0,0.0,-1.0),
-            radius: 0.5,
-            material:
-                Material::DielectricMaterial{
-                    albedo: Vec3::new(1.0,1.0,1.0),
-                    ior: 1.5, 
-                }
-        }),
-        Box::new(Sphere{
-            center: Vec3::new(-1.0,0.0,-1.0),
-            radius: -0.4,
-            material:
-                Material::DielectricMaterial{
-                    albedo: Vec3::new(1.0,1.0,1.0),
-                    ior: 1.5, 
-                }
-        }),
-        Box::new(Sphere{
-            center: Vec3::new(0.0,1.0,-3.0),
-            radius: 0.5,
-            material: Material::EmissiveMaterial{
-                emission_color: Vec3::new(0.8, 0.3, 0.2),
-                strength: 15.0,
-            }
-        }),
-        Box::new(Sphere{
-            center: Vec3::new(0.0,0.0,-1.0),
-            radius: 0.5,
-            material: Material::DiffuseMaterial{
-                albedo: Vec3::new(0.1, 0.2, 0.5),
-            }
-        }),
-        Box::new(Mesh{
-            triangles: cube.triangles,
-            material: Material::MetallicMaterial{
-                albedo: Vec3::new(0.8,0.6,0.2),
-                roughness: 0.0,
-            }
-        }),
-        Box::new(Sphere{
-            center: Vec3::new(0.0,-100.5,0.0),
-            radius: 100.0,
-            material: Material::DiffuseMaterial{
-                albedo: Vec3::new(0.8, 0.8, 0.0),
-            },
-        }),
-    ];
 
-    // setup rendering data
-    let camera_pos = Vec3::new(-3.0, 3.0, 2.0);
-    let target_pos = Vec3::new(0.0, 0.0, -1.0);
-    let depth_of_field = (camera_pos - target_pos).mag();
-    let aperture_size = 0.15;
-
-    let camera = Camera::new(camera_pos, target_pos, 35.0, image_width as f32 / image_height as f32, aperture_size, depth_of_field);
-
-    println!("Rendering {}x{} image @ {} spp; depth {}...", image_width, image_height, samples_per_pixel, max_depth);
+    println!("Rendering {}x{} image @ {} spp; depth {}...", scene.width, scene.height, samples_per_pixel, max_depth);
     
     // limit to one thread
     //rayon::ThreadPoolBuilder::new().num_threads(1).build_global().unwrap();
     
     let mut saving_thread: Option<thread::JoinHandle<()>> = None;
-    let scanlines = (0..image_height).collect::<Vec<i32>>();
+    let scanlines = (0..scene.height).collect::<Vec<u32>>();
     let rendering_start = Instant::now();
+    let next_sample: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(1));
+
+    let ctrl_c_next_sample = next_sample.clone();
+    let ctrl_c_image = protected_image.clone();
+    ctrlc::set_handler(move || {
+        ctrl_c_image.lock().unwrap()
+            .apply_filter(|x| average_samples(ctrl_c_next_sample.load(std::sync::atomic::Ordering::Relaxed), x))
+            .apply_filter(|x| gamma_correct(2.0, x))
+            .save_to_file("/tmp/test.ppm").unwrap();
+    })
+    .expect("Error setting Ctrl-C handler");
+
+
+
     for sample in 0..samples_per_pixel{
         print!("\r[{:>width$}/{:>width$}][{:>6.2}%]{} Rendering...{:>10}",
                 sample+1, samples_per_pixel,
@@ -88,34 +42,32 @@ fn main() {
                 width = samples_per_pixel.to_string().len()
         );
         stdout().flush().unwrap();
-        scanlines.par_iter().for_each_with(protected_image.clone(), |protected_image, y: &i32|{ 
-            let mut this_row: Vec<Vec3> = vec![Vec3::new(0.0, 0.0, 0.0); image_width as usize];
-            for x in 0..image_width{
+        scanlines.par_iter().for_each_with(protected_image.clone(), |protected_image, y: &u32|{ 
+            let mut this_row: Vec<Vec3> = vec![Vec3::new(0.0, 0.0, 0.0); scene.width as usize];
+            for x in 0..scene.width{
                 let x_offset: f32 = rand::random(); 
                 let y_offset: f32 = rand::random(); 
 
-                let u = (x as f32 + x_offset) / image_width as f32;
-                let v = (*y as f32 + y_offset) / image_height as f32;
+                let u = (x as f32 + x_offset) / scene.width as f32;
+                let v = (*y as f32 + y_offset) / scene.height as f32;
 
-                let ray = camera.get_ray(u, v);
+                let ray = scene.camera.get_ray(u, v);
 
                 this_row[x as usize] += trace_ray::trace_ray(ray, &scene, max_depth);
             }
             let mut image = protected_image.lock().unwrap();
-            for x in 0..image_width{
+            for x in 0..scene.width{
                 image[(x, *y)] += this_row[x as usize];
             }
         });
         if sample % 40 == 0{
             let mut image_copy = protected_image.lock().unwrap().clone();
+            let samples_until_now = sample + 1;
             saving_thread = Some(thread::spawn(move || {
-                // average all samples
-                for x in 0..image_width{
-                    for y in 0..image_height{
-                        image_copy[(x, y)] /= (sample+1) as f32;
-                    }
-                }
-                image_copy.save_to_file("/tmp/test.ppm").unwrap();
+                image_copy 
+                    .apply_filter(|x| average_samples(samples_until_now, x))
+                    .apply_filter(|x| gamma_correct(2.0, x))
+                    .save_to_file("/tmp/test.ppm").unwrap();
             }));
         }
         match &saving_thread{
@@ -126,16 +78,13 @@ fn main() {
             },
             None => {}, 
         };
+        next_sample.store(sample + 1, std::sync::atomic::Ordering::Relaxed);
     }
     println!();
     println!("Rendering took {:.2?}", rendering_start.elapsed());
 
-    // average all samples
-    let mut image = protected_image.lock().unwrap();
-    for x in 0..image_width{
-        for y in 0..image_height{
-            image[(x, y)] /= samples_per_pixel as f32;
-        }
-    }
-    image.save_to_file("/tmp/test.ppm").unwrap();
+    protected_image.lock().unwrap()
+        .apply_filter(|x| average_samples(next_sample.load(std::sync::atomic::Ordering::Relaxed), x))
+        .apply_filter(|x| gamma_correct(2.0, x))
+        .save_to_file("/tmp/test.ppm").unwrap();
 }
