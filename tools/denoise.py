f626564aefa89b032435bb79f78e152ec1c373c4
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2023-06-26 06:54:35 +0200

    Added support for animations.

--- tools/denoise.py
+++ tools/denoise.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('/tmp/test.ppm')
+img = cv.imread('/tmp/test0.ppm')
 dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
 cv.imwrite("/tmp/test.denoised.ppm", dst)
 
-# plt.subplot(121),plt.imshow(img)
+#plt.subplot(121),plt.imshow(img)
 # plt.subplot(122),plt.imshow(dst)
-# plt.show()
+plt.show()
