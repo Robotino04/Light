19de413a99d3880b067e2d703c0167b9d3329a91
Robotino04 <roman+github@moonsmile.ch>
2023-06-28 11:03:23 +0200

    Made the cornell box identical to the book.

--- tools/denoise.py
+++ tools/denoise.py
@@ -1,10 +1,16 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('/tmp/test0.ppm')
-dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
-cv.imwrite("/tmp/test.denoised.ppm", dst)
+
+import sys
+
+for file in sys.argv[1:]:
+    img = cv.imread(file)
+    dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+    export_path = file.split(".")
+    export_path[-2] += "_denoised"
+    cv.imwrite(".".join(export_path), dst)
 
 #plt.subplot(121),plt.imshow(img)
-# plt.subplot(122),plt.imshow(dst)
-plt.show()
+#plt.subplot(122),plt.imshow(dst)
+#plt.show()
